489af5dcef02692d6d1c627352174e40928987b8
macbookpro <mahsinsaifullah@gmail.com>
2022-09-21 20:16:39 +0100

    fix: remove apostrophy from banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -11,8 +11,8 @@ export const Banner: React.FC = () => {
           is a place to write, read and connect
         </h1>
         <h2>
-          It's easy and free to post your thinking on any topic and connect with
-          millions of readers.
+          It is easy and free to post your thinking on any topic and connect
+          with millions of readers.
         </h2>
       </div>
       <img
